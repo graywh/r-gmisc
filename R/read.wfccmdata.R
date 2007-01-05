204147c105e4804f19e3d4aacbba59ef1ca1ffee
Will Gray <w.gray@vanderbilt.edu>
2007-01-05 17:10:51 +0000

    Updates on 2007-01-05
    Expand set of character strings to symbolize missing data.
    Strip leading and trailing whitespace from clinical info.

--- R/read.wfccmdata.R
+++ R/read.wfccmdata.R
@@ -1,6 +1,6 @@
 read.wfccmdata <- function(file, sep = "\t")
 {
-    data <- read.table(file, header = TRUE, sep, na.strings = ".", row.names = 1)
+    data <- read.table(file, header = TRUE, sep, na.strings = c(".", "NA", "NaN"), row.names = 1)
     data <- data[,colnames(data) != "id"]
     return(data.frame(t(data)))
 }
--- R/read.wfccminfo.R
+++ R/read.wfccminfo.R
@@ -1,4 +1,4 @@
 read.wfccminfo <- function(file, sep="\t")
 {
-    return(read.table(file, header = TRUE, sep, na.strings = ".", row.names = 1, comment.char = ""))
+    return(read.table(file, header = TRUE, sep, na.strings = c(".", "", "NA", "NaN"), row.names = 1, comment.char = "", strip.white = TRUE))
 }
