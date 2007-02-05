b8f656b77462574370f63c799fe3b313de1542a4
Will Gray <w.gray@vanderbilt.edu>
2007-02-05 22:11:25 +0000

    Updates on 2007-02-05
    Account for the loss of dimension when 'apply' result is 1-by-n or n-by-1.
    Treat data without dimension as vector (margin = 0).

--- R/normalize.R
+++ R/normalize.R
@@ -1,7 +1,7 @@
 normalize <- function(x, margin = c(0,1,2), rename = TRUE)
 {
     margin <- match.arg(margin)
-    if (margin == 0)
+    if (margin == 0 || is.null(dim(x)))
     {
         result <- (x - mean(x))/sd(x)
     }
@@ -10,6 +10,9 @@ normalize <- function(x, margin = c(0,1,2), rename = TRUE)
         result <- apply(x, margin, normalize)
         if (margin == 1)
             result <- t(result)
+        if (!identical(dim(result), dim(x)))
+            dim(result) <- dim(x)
+        dimnames(result) <- dimnames(x)
         if (rename)
             dimnames(result)[[margin]] <- paste(dimnames(result)[[margin]], "std", sep=".")
     }
