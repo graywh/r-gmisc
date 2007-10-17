8d9bd733395cf1efa1c5960d388e6f51f5dcb9ff
Will Gray <w.gray@vanderbilt.edu>
2007-10-17 15:51:44 +0000

    Add new round-robin schedule algorithms

--- R/roundrobin.R
+++ R/roundrobin.R
@@ -1,5 +1,10 @@
-roundrobin <- function(teams, rounds=teams-1)
+roundrobin <- function(teams, rounds=teams - (teams %% 2 == 0), method=c("yahoo","loop","recursive"))
 {
+    if (teams < 4)
+        stop("must have at least 4 teams")
+    if (rounds < 1)
+        stop("must have at least 1 round")
+    method <- match.arg(method)
     t0 <- NA
     bye <- teams %% 2 == 1
     if (bye)
@@ -12,22 +17,60 @@ roundrobin <- function(teams, rounds=teams-1)
     }
     t1 <- teams - 1
     t2 <- teams - 2
-    v <- 1:t1
+    v1 <- 1:t1
+    v2 <- 0:t2
+    b <- 2 * 1:(t2 / 2)
 
     slice <- function(x, n)
     {
         x[(1:length(x) + n - 2) %% length(x) + 1]
     }
 
-    #mat <- matrix(rep(v, t1) - rep(v, each=t1) + 1, ncol=t1) %% t1 + 1
-    mat <- sapply(slice(t1:1, t2), slice, x=v)
-    idx <- mapply(match, v, as.data.frame(mat))
-
-    mat <- replace(mat, idx + 0:t2 * t1, t0)
-
-    if (!bye)
+    if (method == "recursive")
     {
-        mat <- cbind(mat, order(idx))
+        if ((log2(teams) %% 1) > 0)
+            stop("recursive requires 2^n or 2^(n-1) teams.")
+        if (teams == 4)
+            mat <- roundrobin(4, method="yahoo")
+        else
+        {
+            td2 <- teams / 2
+            #vd2 <- 1:td2
+            smat <- roundrobin(td2, method="recursive")
+            smat2 <- smat + td2
+            #mat <- rbind(cbind(smat, smat2), c(vd2 + td2, vd2), cbind(smat2, smat))
+            mat <- rbind(cbind(smat, smat2), slice(1:teams, td2 + 1), cbind(smat2, smat))
+        }
+        if (bye)
+        {
+            idx <- c(t1, b, b-1)
+            mat <- replace(mat, idx + v2 * t1, t0)
+            mat <- mat[,v1]
+        }
+    }
+    else if (method == "loop")
+    {
+        mat <- roundrobin(teams, method="yahoo")
+        mat <- mat[order(-mat[,teams]), c(teams, 1:t1)] %% teams + 1
+
+        if (bye)
+        {
+            idx <- which(mat == teams)
+            mat <- replace(mat, idx, t0)
+            mat <- mat[,v1]
+        }
+    }
+    else # yahoo, loop
+    {
+        #mat <- matrix(rep(v, t1) - rep(v, each=t1) + 1, ncol=t1) %% t1 + 1
+        mat <- sapply(slice(t1:1, t2), slice, x=v1)
+        idx <- c(t1, b, b-1)
+        mat <- replace(mat, idx + v2 * t1, t0)
+
+        if (!bye)
+        {
+            mat <- cbind(mat, order(idx))
+        }
     }
 
     mat[(1:rounds - 1) %% t1 + 1,]
