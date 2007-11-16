e4a6f7845c185717c4e10175e31bd0a64c3adf5e
Will Gray <w.gray@vanderbilt.edu>
2007-11-16 18:16:28 +0000

    Remove items that were moved to package Wfccm

--- R/read.wfccmdata.R
+++
@@ -1,6 +0,0 @@
-read.wfccmdata <- function(file, sep = "\t", ...)
-{
-    data <- read.table(file, header=TRUE, sep, na.strings=c(".", "NA", "NaN"), row.names=1, comment.char="", strip.white=TRUE, ...)
-    data <- data[,colnames(data) != "id"]
-    return(data.frame(t(data)))
-}
