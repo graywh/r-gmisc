bdedf2610df83ac56d4d07cb7388602c7d35987d
Will Gray <w.gray@vanderbilt.edu>
2008-03-03 21:05:58 +0000

    Updates on 2008-03-03]
    Update base64 functions to work on vectors.
    Remove ASCII functions in favor of R's built-in charToRaw and rawToChar.
    Remove redundant author info from help files.

--- R/base64.R
+++ R/base64.R
@@ -2,37 +2,47 @@ BASE64 <- c(LETTERS, letters, as.character(0:9), "+", "/")
 
 encode64 <- function(x)
 {
-    l <- ceiling(length(x) / 3)
-    a <- length(x) %% 3
-    y <- c()
-    m <- 256 ^ (2:0)
-    d <- 64 ^ (3:0)
-    for (i in 1:l)
-    {
-        t <- x[(i*3-2):(i*3)] * m
-        t[is.na(t)] <- 0
-        y[(i*4-3):(i*4)] <- sum(t) %/% d %% 64
-    }
-    y <- BASE64[y + 1]
-    if (a > 0)
-        y[(l*4-2+a):(l*4)] <- "="
-    return(paste(y, collapse=""))
+    sapply(x, function(x) {
+        if (class(x) == "character")
+            x <- as.numeric(charToRaw(x))
+        l <- ceiling(length(x) / 3)
+        a <- length(x) %% 3
+        y <- c()
+        m <- 256 ^ (2:0)
+        d <- 64 ^ (3:0)
+        for (i in 1:l)
+        {
+            t <- x[(i * 3 - 2):(i * 3)] * m
+            t[is.na(t)] <- 0
+            y[(i * 4 - 3):(i * 4)] <- sum(t) %/% d %% 64
+        }
+        y <- BASE64[y + 1]
+        if (a > 0)
+            y[(l * 4 - 2 + a):(l * 4)] <- "="
+        paste(y, collapse="")
+    })
 }
 
-decode64 <- function(x)
+decode64 <- function(s, toChar=TRUE)
 {
-    x <- strsplit(x, "")[[1]]
-    l <- length(x) / 4
-    x <- x[x != "="]
-    x <- match(x, BASE64) - 1
-    y <- c()
-    m <- 64 ^ (3:0)
-    d <- 256 ^ (2:0)
-    for (i in 1:l)
-    {
-        t <- x[(i*4-3):(i*4)] * m
-        t[is.na(t)] <- 0
-        y[(i*3-2):(i*3)] <- sum(t) %/% d %% 256
-    }
-    return(y)
+    s <- strsplit(s, "")
+    sapply(s, function(x) {
+        l <- length(x) / 4
+        a <- sum(x == "=")
+        x <- x[x != "="]
+        x <- match(x, BASE64) - 1
+        y <- c()
+        m <- 64 ^ (3:0)
+        d <- 256 ^ (2:0)
+        for (i in 1:l)
+        {
+            t <- x[(i * 4 - 3):(i * 4)] * m
+            t[is.na(t)] <- 0
+            y[(i * 3 - 2):(i * 3)] <- sum(t) %/% d %% 256
+        }
+        if (a > 0)
+            y <- y[1:(length(y) - a)]
+        if (toChar)
+            rawToChar(as.raw(y))
+    })
 }
