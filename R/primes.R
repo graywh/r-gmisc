1ef3f95a1d8ee84d85d110a6f6d25f35a23bff10
Will Gray <w.gray@vanderbilt.edu>
2008-05-29 15:48:41 -0500

    Tatuski's prime factorization function

---
+++ R/primes.R
@@ -0,0 +1,22 @@
+prime.factorization <- function(x) {
+    sqx <- sqrt(x)
+    numbers <-  if (x < 100) {
+                    seq(2,sqx)
+                } else {
+                    c(2, 3, seq(5, sqx, by=6), seq(7, sqx, by=6))
+                }
+    candid <- numbers[(x %% numbers) == 0]
+    rem <- outer(candid, candid, '%%')
+    fac <- apply(rem, 1, function(x) { sum(x==0) })
+    prim.fac <- candid[fac == 1]
+
+    how.many.times <- function(y, b) {
+        sum(y %% b^(1:log(y, base=b)) == 0)
+    }
+
+    pow <- unlist(lapply(prim.fac, how.many.times, y=x))
+    maybe <- x / prod(ppa <- prim.fac ^ pow)
+    out <- cbind(prim.fac, pow)
+    if (maybe > 1) { out <- rbind(out, c(maybe, 1)) }
+    return(out[order(out[,1]),])
+}
