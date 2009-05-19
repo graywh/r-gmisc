46291a9e6264004b375ad84f9f069b0f22ff678c
Will Gray <w.gray@vanderbilt.edu>
2009-05-19 16:24:36 -0500

    Some work on prime numbers I did for Tatsuki

--- R/primes.R
+++ R/primes.R
@@ -33,3 +33,79 @@ sieve.of.eratosthenes <- function(limit) {
     }
     return(which(is.prime))[-1]
 }
+
+is.prime.naive <- function(x) {
+    x <- abs(x)
+    k <- c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53)
+    if (x == 1) return(FALSE)
+    if (x %in% k) return(TRUE)
+    if (any(x %% k == 0)) return(FALSE)
+    sqx <- floor(sqrt(x))
+    for (i in seq(5, sqx+1, 6)) {
+        if (x %% i == 0) return(FALSE)
+        if (x %% (i+2) == 0) return(FALSE)
+    }
+    return(TRUE)
+}
+
+miller.rabin <- function(x, k=NULL) {
+    pow <- function(x, y, z) { (x ^ y) %% z }
+    x <- abs(x)
+    if (x == 1) return(FALSE)
+    if (x == 2) return(TRUE)
+    if (x %% 2 == 0) return(FALSE)
+    x1 <- x - 1
+    d <- x1
+    s <- 0
+    while (d %% 2 == 0) {
+        d <- d / 2
+        s <- s + 1
+    }
+    if (is.null(k)) {
+        for (a in seq(2, min(x1, floor(2 * log(x) ^ 2))))
+            if (pow(a,d,x) != 1 && all(pow(a, (2 ** seq(0, s-1)) * d, x) != x1))
+                return(FALSE)
+    } else {
+        for (j in seq(0, k-1)) {
+            a <- sample(x1, 1)
+            if (pow(a,d,x) != 1 && all(pow(a, (2 ** seq(0, s-1)) * d, x) != x1))
+                return(FALSE)
+        }
+    }
+    return(TRUE)
+}
+
+nearprimes <- function(x) {
+    if (is.prime.naive(x)) {
+        a <- x
+        b <- x
+    } else {
+        a <- nextprime(x)
+        b <- prevprime(x)
+    }
+    return(c(b,a))
+}
+
+prevprime <- function(x) {
+    prevn <- function(n) {
+        if (n == 0) return(1)
+        if (n == 1) return(2)
+        return(n - 1)
+    }
+    y <- x - prevn(x %% 6)
+    while (!is.prime.naive(y))
+        y <- y - prevn(y %% 6)
+    return(y)
+}
+
+nextprime <- function(x) {
+    nextn <- function(n) {
+        if (n == 0) return(1)
+        if (n == 5) return(2)
+        return(5 - n)
+    }
+    y <- x + nextn(x %% 6)
+    while (!is.prime.naive(y))
+        y <- y + nextn(y %% 6)
+    return(y)
+}
