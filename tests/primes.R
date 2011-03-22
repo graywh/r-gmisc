d233baf3ac3f143c35b79f2718458da59ce3258f
Will Gray <w.gray@vanderbilt.edu>
2011-03-22 10:30:00 -0500

    Tests for prime.factorization

---
+++ tests/primes.R
@@ -0,0 +1,10 @@
+library(Gmisc, warn.conflicts=FALSE)
+
+# 6125
+with(prime.factorization(6125), prod(factors**powers))
+
+# 231
+with(prime.factorization(231), prod(factors**powers))
+
+# 35336848261
+with(prime.factorization(35336848261), prod(factors**powers))
